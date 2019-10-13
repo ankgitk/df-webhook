7a082c9c4e4c8b065f9fa1839858352d8629aa29
Ankit Kumar <30922723+ankgitk@users.noreply.github.com>
2019-10-14 05:04:03 +0530

    Create jsondb.py

---
+++ jsondb.py
@@ -0,0 +1,30 @@
+replies =  [
+    'Savings',
+    'Checking',
+    'Investments',
+]
+
+CONDITION_DICT = {
+    'rain': 'chanceofrain',
+    'snow': 'chanceofsnow',
+    'wind': 'chanceofwindy',
+    'sun': 'chanceofsunshine',
+    'fog': 'chanceoffog',
+    'foggy': 'chanceoffog',
+    'thunderstorm': 'chanceofthunder',
+    'overcast': 'chanceofovercast',
+    'clouds': 'cloudcover',
+}
+
+SUPPORTED = [
+    'rain',
+    'snow',
+    'wind',
+    'sun',
+    'fog',
+    'thunderstorm',
+    'overcast',
+    'clouds',
+    'foggy',
+]
+
